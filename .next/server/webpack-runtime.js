be8c6a2bead9f490c790cca54234266b4346e9d3
Soufiane Bendhiba <bendhiba.soufiane@gmail.com>
2025-06-30 01:40:38 +0100

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e1a7496a25b58010")
+/******/ 		__webpack_require__.h = () => ("d7d9d9ff52767bee")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
