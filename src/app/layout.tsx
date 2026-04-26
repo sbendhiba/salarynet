6b2fe621e62a1b819ef5ef26119b814e282be65f
Soufiane Bendhiba <bendhiba.soufiane@gmail.com>
2026-04-26 22:35:55 +0100

    Add Google Analytics tracking scripts to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://salairenet.ma'),
@@ -64,7 +65,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-371NB0C9WY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-371NB0C9WY');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
